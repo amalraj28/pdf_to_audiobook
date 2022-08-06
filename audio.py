d8ed3b1ae4d261cb7e29dfac7ae0eced35c5d666
Amal Raj <99964089+amalraj28@users.noreply.github.com>
2022-08-06 13:01:42 +0530

    Update audio.py
    Updated the file to allow to save the audio as a local file. Also voice can be changed and the rate of speaking can be changed.

--- audio.py
+++ audio.py
@@ -1,7 +1,16 @@
 import pyttsx3  # Built-in module to read text
 
+
 def audio(text):
     speaker = pyttsx3.init()
+    voices = speaker.getProperty('voices')
+    speaker.setProperty('voice', voices[3].id)
+    speaker.setProperty('rate', 150)
     speaker.say(text)
+    speaker.save_to_file(text, 'CTS2.wav')
     speaker.runAndWait()
 
+# To print the list of voices:
+# voices = speaker.getProperty('voices')
+# for voice in voices:
+#   print(f'{voice}')
